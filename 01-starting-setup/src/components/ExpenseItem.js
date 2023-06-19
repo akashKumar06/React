8deacb54b85de233c1617468883eceb73903e49f
Akash <akashkumar59999@gmail.com>
2023-06-19 12:44:55 +0530

    Adding basic styles and using props

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+    const expenseDate = new Date();
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 345.5;
+    return (
+        <div className = "expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className = 'expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className = 'expense-item__price'>{expenseAmount}</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItem;
