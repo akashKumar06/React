f16e63eec381cbfeba945b42601a209d546b012d
Akash <akashkumar59999@gmail.com>
2023-06-21 22:11:42 +0530

    finally done with project2

--- 02-Project/src/App.js
+++ 02-Project/src/App.js
@@ -29,29 +29,7 @@ function App() {
   return (
     <div>
       <Header/>
-      {/* <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header> */}
       <Concept concepts = {concepts}/>
-      {/* <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt="TODO: TITLE" />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt="TODO: TITLE" />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt="TODO: TITLE" />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
-      </ul> */}
     </div>
   );
 }
