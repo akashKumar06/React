6be32e4eaf645139c183a59d8a5a90c21cea1a5b
Akash <akashkumar59999@gmail.com>
2023-07-04 17:31:11 +0530

    listing

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -39,7 +39,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense = {addExpenseHandler}/>
-      <Expenses items = {expenses }/>
+      <Expenses items = {expenses}/>
     </div>
   );
 }
