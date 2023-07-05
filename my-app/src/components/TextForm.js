0b637d630631fb4ffd0744f9476c756a4fdccb08
Akash <akashkumar59999@gmail.com>
2023-07-05 21:35:27 +0530

    doing good

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -5,10 +5,16 @@ import React,{useState} from 'react';
 const TextForm = (props) => {
     const [text, setText] = useState('Enter text here');
 
-    const handleUpperCaseClick = (str)=>{
+    const handleUpperCaseClick = ()=>{
         console.log("Uppercase Clicked");
         let newText = text.toUpperCase();
         setText(newText); 
+    }
+
+    const handleLowerCaseClick = () => {
+        console.log("lowercase clicked");
+        let newText = text.toLowerCase();
+        setText(newText)
     }
 
     const handleOnChange = (event) => {
@@ -22,6 +28,7 @@ const TextForm = (props) => {
                 <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpperCaseClick}>Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleLowerCaseClick}>Convert to Lowercase</button>
         </div>            
     )
 
