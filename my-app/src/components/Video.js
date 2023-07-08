14be29fd8769a4f7ddebcd9fa7292cd4c223d0ac
Akash <akashkumar59999@gmail.com>
2023-07-08 16:04:52 +0530

    going with the flow

--- my-app/src/components/Video.js
+++ my-app/src/components/Video.js
@@ -1,6 +1,10 @@
-
-function Video(){
-    return <div>Video</div>
+function Video() {
+  return (
+    <div>
+      <img src="https://loremflickr.com/160/90" alt="Kuch tho hai" />
+      <div>React JS Tutorials</div>
+    </div>
+  );
 }
 
 export default Video;
