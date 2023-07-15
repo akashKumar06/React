cf68927e555574e61b07316b2061aa2ceff45873
Akash <akashkumar59999@gmail.com>
2023-07-15 08:49:27 +0530

    going

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,21 @@
-import Video from "./components/Video";
 import "./App.css";
-import videoDB from "./data/data.js";
-import PlayButton from "./components/PlayButton";
-import { useState } from "react";
 import AddVideos from "./components/AddVideos";
-function App({ addVideos }) {
+import VideoList from './components/VideoList';
+import React,{useState}from 'react'
+
+import videoDB from "./data/data.js";
+
+function App() {
   const [videos, setVideos] = useState(videoDB);
 
   function handleAddVideo(video) {
     setVideos([...videos, video]);
   }
-
+  
   return (
     <div className="App">
       <AddVideos onAddVideo={handleAddVideo} />
-      {videos.map((video) => (
-        <Video
-          key={video.id}
-          title={video.title}
-          views={video.views}
-          time={video.time}
-          channel={video.channel}
-          verified={video.verified}
-          id={video.id}
-        >
-          <PlayButton
-            onPlay={() => {
-              console.log("Playing", video.title);
-            }}
-            onPause={() => console.log("Paused", video.title)}
-          >
-            {video.title}
-          </PlayButton>
-        </Video>
-      ))}
+      <VideoList videos={videos}/>
     </div>
   );
 }
--- my-app/src/components/AddVideos.js
+++ my-app/src/components/AddVideos.js
@@ -1,23 +1,28 @@
 import "./AddVideos.css";
 import { useState } from "react";
 
-function AddVideo({onAddVideo}) {
-  const [video, setVideo] = useState({
-    time: "3 months ago",
-    channel: "Akash Kumar",
-    verified: "true",
-  });
+const initialState = {
+  time: "3 months ago",
+  channel: "Akash Kumar",
+  verified: "true",
+  title:'',
+  views:''
+};
+
+function AddVideo({ onAddVideo }) {
+  const [video, setVideo] = useState(initialState);
 
   function handleSubmit(e) {
     e.preventDefault();
     onAddVideo(video);
+    setVideo(initialState);
   }
 
   function handleChange(e) {
     e.stopPropagation();
     setVideo({
       ...video,
-      id:Math.floor((Math.random()*100 + 1)),
+      id: Math.floor(Math.random() * 100 + 1),
       [e.target.name]: e.target.value,
       [e.target.name]: e.target.value,
     });
@@ -37,9 +42,7 @@ function AddVideo({onAddVideo}) {
         placeholder="views"
         onChange={handleChange}
       />
-      <button>
-        Add Videos
-      </button>
+      <button>Add Videos</button>
     </form>
   );
 }
