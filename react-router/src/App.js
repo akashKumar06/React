546b10a856e981e6ff5a52ba407c69a1b718c661
Akash Kumar <akashkumar59999@gmail.com>
2023-08-27 10:46:57 +0530

    react-router

---
+++ react-router/src/App.js
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>}/>
+//     <Route path="/products" element={<ProductsPage/>}/>
+//   </Route>
+// )
+// const router = createBrowserRouter(routeDefinitions);
+
+const router = createBrowserRouter([
+  {
+    path: "/", //root route
+    element: <RootLayout />, //layout page
+    errorElement: <ErrorPage/>,
+    children: [ 
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
