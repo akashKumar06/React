d4405033917b0b71719f9e6cc1da711115a6b88a
Akash Kumar <akashkumar59999@gmail.com>
2023-08-27 15:10:32 +0530

    useParams,NavLink,index,relative and absolute paths, useNavigate

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -17,10 +18,11 @@ const router = createBrowserRouter([
   {
     path: "/", //root route
     element: <RootLayout />, //layout page
-    errorElement: <ErrorPage/>,
-    children: [ 
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
+    errorElement: <ErrorPage />,
+    children: [
+      { index:true, element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
